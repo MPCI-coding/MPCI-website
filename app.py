50636d5124d490d4dea1f5ae4352470d02ea2aa4
Clements Scott-MAP <map10038@jpn.marubeni-mpci.com>
2024-01-22 15:22:32 -0500

    Created base.html template that all the other pages will inherit from.

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_babel import Babel
 
+def get_locale():
+    return request.args.get('lang') or request.accept_languages.best_match(['en', 'ja'])
+
 app = Flask(__name__)
 babel = Babel(app)
+babel.init_app(app, locale_selector=get_locale)
 
 # Babel configuration
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
-@babel.localeselector
-def get_locale():
-    return request.args.get('lang') or request.accept_languages.best_match(['en', 'ja'])
+
+
 
 # Routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return  render_template('index.html')
 
 @app.route('/about')
 def about():
@@ -34,4 +37,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=9000)
